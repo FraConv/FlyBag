4a97c4227e62625e4326b06bd4155c5882a0264d
DrakeTheRed <campanella.giulio03@gmail.com>
2025-05-22 21:02:43 +0200

    Removed Tab1.css because empty, added Login page (still wip), added basic theme definitions in variables.css

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,14 @@
 import { IonPage } from "@ionic/react";
 import React from "react"
+import "./Login.css"
+import logo from "./logo.svg"
 
 const Login: React.FC = () => {
     return (
-        <IonPage>
-           
+        <IonPage id="main">
+           <img src={logo} alt="Nope" />
+           <h1>Benvenuto in <span>Flybag</span></h1>
+           <p>Let me penetrate you</p>
         </IonPage>
     );
 }
